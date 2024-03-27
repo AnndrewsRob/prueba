e55bcb98d795fa771cc1ef3844e2b6bf3ce9c29f
DrewRob <96999691+AnndrewsRob@users.noreply.github.com>
2024-03-27 18:31:58 -0500

    Add files via upload

--- idiomind/settings.py
+++ idiomind/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = getenv('DJANGO_SECRET_KEY',get_random_secret_key())
 DEBUG = getenv('DEBUG','False') == 'True'
 
 
-ALLOWED_HOSTS=getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS=getenv('DJANGO_ALLOWED_HOSTS','127.0.0.1,localhost','prueba-production-8e48.up.railway.app').split(',')
 
 
 
